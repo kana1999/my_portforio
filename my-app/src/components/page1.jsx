32586449446e9a920960d4cb489387a58df692eb
kana <kana@MacBook2016.local>
2024-08-15 10:32:03 +0900

    ログインフォーム修正

--- my-app/src/components/page1.jsx
+++ my-app/src/components/page1.jsx
@@ -4,20 +4,20 @@ import React from "react";
 export const Page1 = () => {
         return (
             <div>
-            <div className="contact-form">
-                <form>
-                    <p>メールアドレス</p>
-                    <input />
-                    <p>パスワード</p>
-                    <input />
-                    <br />
-                    <input
-                    type="submit"
-                    value="ログイン"
-                    />
-                </form>
-            </div>
-            <Link to="/Home">サイトトップに戻る</Link>
+                <div className="contact-form">
+                    <form>
+                        <p>メールアドレス</p>
+                        <input />
+                        <p>パスワード</p>
+                        <input />
+                        <br />
+                        <input
+                        type="submit"
+                        value="ログイン"
+                        />
+                    </form>
+                </div>
+                <Link to="/Home">サイトトップに戻る</Link>
             </div>
         );
 }
