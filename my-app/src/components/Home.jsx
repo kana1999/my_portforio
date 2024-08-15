a7206fb8b69f19b3d6551c8125f1293402eb27d1
kana <kana@MacBook2016.local>
2024-08-16 00:08:07 +0900

    説明ページ再編集

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -9,7 +9,7 @@ class Home extends React.Component {
             <div className="Container">
                     <div className="Container-header">
                         <h1>旅のしおりで旅行がさらに楽しめる！</h1>
-                        <p>旅のしおりの作成と共有ができる〇〇です。無料かつアプリダウンロードも不要でブラウザがあれば旅行プランが作成可能。「女子旅」「卒業旅行」「社員旅行」「家族旅行」「一人旅」「修学旅行」の計画を立てよう。</p>
+                        <p>旅のしおりの作成と共有ができる〇〇です。無料かつアプリダウンロードも不要！ブラウザがあれば旅行プランが作成可能です。「女子旅」「卒業旅行」「社員旅行」「家族旅行」「一人旅」「修学旅行」の計画を立ててみませんか？</p>
                     </div>
                     <br />
                         <div className="Container-list list-1">
@@ -17,7 +17,7 @@ class Home extends React.Component {
                                 <img className="C-li" src="https://loosedrawing.com/assets/illustrations/png/701.png"></img>
                                 <h1>旅行プランを手軽に作成</h1>
                                 <br />
-                                <p>テンプレートが用意されているので作りやすい</p>
+                                <p>テンプレートが用意されているので作りやすい！</p>
                                 <br />
                                 <p>他のユーザーが作ったプランも見られるので、参考にできます。</p>
                                 <br />
@@ -33,20 +33,25 @@ class Home extends React.Component {
                                 <p>プランを作成したら、家族や友だちにURLで共有できます。</p>
                                 <br />
                                 <p>LINEのトークやノートに貼っておくと便利です。アプリ不要でブラウザがあれば見れるのでSNSでも共有しやすいです。</p>
+                                <br />
+                                <p>スケジュールにいいね機能も付いているので候補に迷ったら投票して決めることも！</p>
                             </div>
                 </div>
-                <Link to="/Page1">新規登録・ログインする</Link>
+                <button><Link to="/Page1" style={{textDecoration: 'none' ,color: 'black', padding: '20px'}}>新規登録・ログインする</Link></button>
                 <br />
-                <h1>使い方は？</h1>
-                <br />
-                <h3>〜旅のしおりを作成〜</h3>
-                <br />
-                <p>旅行プランのスケジュールを作成</p>
-                <p>行く場所や泊まる場所、URL…etcを入力し、「登録」ボタンを押して完成。何度も編集可能なので急なスケジュール変更にも対応できます。</p>
-                <br />
-                <h3>〜旅行仲間に旅のしおりをURLで共有〜</h3>
-                <br />
-                <p>しおりが完成したら旅行仲間にURLで共有できます。LINEのノートに貼ったり、ブックマークしておくとすぐに見れて便利です。アプリをダウンロードする必要がないのでスムーズに確認できます。</p>
+                <div className="HowToUse">
+                    <h1>使い方は？</h1>
+                    <br />
+                    <h3>〜旅のしおりを作成〜</h3>
+                    <br />
+                    <p>旅行プランのスケジュールを作成</p>
+                    <p>行く場所や泊まる場所、URL…etcを入力し、「登録」ボタンを押して完成。何度も編集可能なので急なスケジュール変更にも対応できます。</p>
+                    <br />
+                    <h3>〜旅行仲間に旅のしおりをURLで共有〜</h3>
+                    <br />
+                    <p>しおりが完成したら旅行仲間にURLで共有できます。LINEのノートに貼ったり、ブックマークしておくとすぐに見れて便利です。アプリをダウンロードする必要がないのでスムーズに確認できます。</p>
+                </div>
+                <button><Link to="/Page1" style={{textDecoration: 'none' ,color: 'black', padding: '20px'}}>新規登録・ログインする</Link></button>
             </div>
         </section>
     );
