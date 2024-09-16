7aa37a2a22faaeb70ad3f215c2d1423cb6f9daac
kana <kana@MacBook2016.local>
2024-09-16 20:28:01 +0900

    プレビュー追加

--- my-app/src/components/page3.jsx
+++ my-app/src/components/page3.jsx
@@ -1,191 +1,187 @@
 import { Link } from "react-router-dom";
-import React, { useState, useCallback } from "react";
+import { useForm, useFieldArray } from 'react-hook-form';
+import React, { useEffect } from "react";
 import './page3.css';
 
 export const Page3 = () => {
-    const [formData, setFormData] = useState([{
+  const { register, handleSubmit, control, watch } = useForm({
+    defaultValues: {
+      formData: [{ 
         title: "",
         time: "",
         caption: ""
-    }]);
-
-    const [details, setDetails] = useState({
-      gender: "",
-      age: "",
-      cost: "",
-      person: ""
-    });
-
-    const addElement = () => {
-      setFormData([...formData, {
-          title: "",
-          time: "",
-          caption: ""
-      }]);
-  };
-
-
-    // handleChange を useCallback でラップ
-    const handleChange = useCallback((e, index) => {
-        const { name, value } = e.target;
-        if (index !== undefined) {
-          // formData の場合
-        setFormData(prevFormData => 
-            prevFormData.map((data, i) => 
-                i === index ? { ...data, [name]: value } : data
-            )
-        );
-      } else {
-        // details の場合
-        setDetails(prevDetails => ({
-            ...prevDetails,
-            [name]: value
-        }));
+      }],
+      details: {
+        PlanName: "",
+        gender: "",
+        age: "",
+        cost: "",
+        person: "",
+        itinerary: ""
       }
-    }, []);
+    },
+  });
 
-    function Savedata() {
-        alert("登録されました！");
-    }
+  const { fields, append, remove } = useFieldArray({
+    name: "formData",
+    control
+  }); 
 
+  // フォームのデータの監視
+  const watchedFormData = watch("formData");
+  const watchedDetails = watch("details");
+
+  useEffect(() => {
+      // フォームデータの変更を監視してコンソールに出力
+      console.log("フォームデータが変更されました:", watchedFormData);
+      console.log("詳細情報が変更されました:", watchedDetails);
+  }, [watchedFormData, watchedDetails]);
+
+  const onSubmit = (data) => {
+      alert("登録されました！");
+      console.log("送信されたデータ:", data);
+  };
+  
     return (
-        <div className="Container" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
-            <div className="PlanExsample">
-                <h1>プラン</h1>
-                <br />
-                <table>
-                  <tbody>
-                    {formData.map((data, index) => (
-                      <div className="PlanCategory" key={index}>
-                        <p>{ index + 1 }日目</p>
-                        <PlanForm formData={data} handleChange={handleChange} index={index}/>
-                    </div>
-                    ))}
-
-                    <tr>
-                      <td colSpan="2">
-                          <br />
-                          <button className="PlanDateil">予定を削除</button>
-                          <br />
-                          <button className="PlanDateil" onClick={ addElement }>予定を追加</button>
-                      </td>
-                    </tr>
-                    {/* gender, age, cost, person のみ一度だけ表示 */}
-                    <tr>
-                      <th><label>人数:</label></th>
-                      <td>
+        <div className="Container">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="Plan-Container">
+              <div className="PlanExsample">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="旅行名"
+                    />
+                  </div>
+                  <div>
+                    <label>プラン名：</label>
+                    <input
+                      type="text"
+                      placeholder="節約プラン"
+                      {...register(`details.PlanName`)}
+                    />
+                  </div>
+                  {/* gender, age, cost, person のみ一度だけ表示 */}
+                  <div className="Plan-details">
+                      <div>
+                        <label>旅行期間:</label>
                           <input
                               type="text"
-                              name="person"
-                              value={details.person}
-                              onChange={(e) => handleChange(e)}
+                              {...register(`details.itinerary`)}
                           />
-                      </td>
-                  </tr>
-                  <tr>
-                      <th><label>性別:
-                          </label>
-                      </th>
-                      <td>
-                          <select name="selectedgender">
+                      </div>
+                      <div>
+                        <label>人数:</label>
+                          <input
+                              type="text"
+                              {...register(`details.person`)}
+                          />
+                      </div>
+                      <div>
+                        <label>性別:</label>
+                          <select {...register(`details.gender`)}>
                               <option value="choice">選択してください</option>
                               <option value="man">男性</option>
                               <option value="woman">女性</option>
                           </select>
-                          {/*<input
-                              type="text"
-                              name="gender"
-                              value={details.gender}
-                              onChange={(e) => handleChange(e)}
-                          />*/}
-                      </td>
-                  </tr>
-                  <tr>
-                      <th><label>年齢:</label></th>
-                      <td>
+                      </div>
+                      <div>
+                        <label>年齢:</label>
                           <input
                               type="text"
-                              name="age"
-                              value={details.age}
-                              onChange={(e) => handleChange(e)}
+                              {...register(`details.age`)}
                           />
-                      </td>
-                  </tr>
-                  <tr>
-                      <th><label>大まかな予算:</label></th>
-                      <td>
+                      </div>
+                      <div>
+                        <label>予算:</label>
                           <input
                               type="text"
-                              name="cost"
-                              value={details.cost}
-                              onChange={(e) => handleChange(e)}
+                              {...register(`details.cost`)}
                           />
-                      </td>
-                  </tr>
-                  </tbody>
-              </table>
-              <button type="submit" className="PlanDateil" id="Savedata">登録</button>
-              <br />
-              <button className="PlanDateil" id="Backpage">
-                    <Link to="/Page2" style={{ textDecoration: 'none', color: 'black', padding: '20px' }}>マイページに戻る</Link>
-              </button>
+                      </div>
+                    </div>
+                    <div>
+                      {fields.map((field, index) => (
+                      <div className="PlanCategory" key={field.id}>
+                        <div className="DayandRemove">
+                          <p className="Day-count">{ index + 1 }日目</p>
+                          <div>
+                            <button type="button" className="removeButton" onClick={() => remove( index )}></button>
+                          </div>
+                        </div>
+                        <PlanForm register={register} index={index}/>
+                      </div>
+                    ))}
+                        <div>
+                          <button type="button" className="PlanDetail" onClick={() => append({ title: "", time: "", caption: "" })}>日付を追加</button>
+                        </div>
+
+                  <button type="submit" className="PlanDetail" id="Savedata">登録</button>
+                </div>
+              </div>
+              <div className="Plan-preview">
+                <h2>プレビュー</h2>
+                  <ul>
+                    <li>プラン名：{watchedDetails.PlanName}</li>
+                    <li>旅行期間：{watchedDetails.itinerary}</li>
+                    <li>予算：{(`¥${watchedDetails.cost}`)}</li>
+                  </ul>
+                <br />
+                <h3>デイリースケジュール</h3>
+                  <p>{watchedFormData.title}</p>
+                  {fields.map((field, index) => (
+                      <div key={field.id}>
+                          <p className="Preview-Day-count">{ index + 1 }日目</p>
+                        <label>概要: {watchedFormData[index].title}</label>
+                      </div>
+                    ))}
+              </div>
             </div>
+          </form>
+          
+              <button className="PlanDetail" id="Backpage">
+                  <Link to="/Page2" 
+                        style={{ textDecoration: 'none', color: 'black', padding: '20px' }}>
+                        マイページに戻る
+                  </Link>
+              </button>
         </div>
     );
 };
 
 // PlanForm コンポーネントの定義
-const PlanForm = ({ formData, handleChange, index }) => {
+const PlanForm = ({ register, index }) => {
   // 新しい要素を追加する関数
 
   return (
     <div className="FormWrapper">
-      <>
-        <tr>
-            <th><label>タイトル:</label></th>
-            <td>
-                <input
-                    type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={(e) => handleChange(e, index)}
-                />
-            </td>
-        </tr>
-        <tr>
-            <th><label>時間:</label></th>
-            <td>
-                <input
-                    type="text"
-                    name="time"
-                    value={formData.time}
-                    onChange={(e) => handleChange(e, index)}
-                />
-            </td>
-        </tr>
-        <tr>
-            <th><label>スケジュール内容:</label></th>
-            <td>
-                <input
-                    type="text"
-                    name="caption"
-                    value={formData.caption}
-                    onChange={(e) => handleChange(e, index)}
-                />
-            </td>
-        </tr>
-        <tr>
-          <td colSpan="2">
+      <div>
+          <label>概要:</label>
+            <input
+                type="text"
+                {...register(`formData.${index}.title`)}
+            />
+      </div>
+      <div>
+          <label>時間:</label>
+            <input
+                type="text"
+                {...register(`formData.${index}.time`)}
+            />
+      </div>
+      <div>
+          <label>スケジュール内容:</label>
+            <textarea
+                type="text"
+                {...register(`formData.${index}.caption`)}
+            />
+      </div>
             <button className="PlanDetail">
               <Link to="/Page4"
-                    style={{ textDecoration: 'none', color: 'black'}}
-              >
+                    style={{ textDecoration: 'none', color: 'black'}}>
                     内容を編集する
               </Link>
             </button>
-          </td>
-        </tr>
-      </>
     </div>
   );
 };
